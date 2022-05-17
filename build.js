fce664478b98c5e9985827ea4c3acd6197afc592
Costanza Volpini <costanza.volpini@gmail.com>
2022-05-17 14:50:27 +0200

    Initial setup + added colors, typography for Apollo and Artemis

---
+++ build.js
@@ -0,0 +1,61 @@
+const StyleDictionaryPackage = require('style-dictionary');
+
+// Auto generate Style Dictionary config file
+StyleDictionaryPackage.registerFormat({
+    name: 'css/variables',
+    formatter: function (dictionary, config) {
+      return `${this.selector} {
+        ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
+      }`
+    }
+  });  
+
+// Transformer for Style Dictionary
+StyleDictionaryPackage.registerTransform({
+    name: 'sizes/px',
+    type: 'value',
+    matcher: function(prop) {
+        return ["fontSize", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
+    },
+    transformer: function(prop) {
+        return parseFloat(prop.original.value) + 'px';
+    }
+    });
+
+function getStyleDictionaryConfig(theme) {
+  return {
+    "source": [
+      `tokens/${theme}.json`,
+    ],
+    "platforms": {
+      "web": {
+        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
+        "buildPath": `output/`,
+        "files": [{
+            "destination": `${theme}.css`,
+            "format": "css/variables",
+            "selector": `.${theme}-theme`
+          }]
+      }
+    }
+  };
+}
+
+console.log('Build started...');
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
+
+['apollo', 'artemis'].map(function (theme) {
+
+    console.log('\n==============================================');
+    console.log(`\nProcessing: [${theme}]`);
+
+    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
+
+    StyleDictionary.buildPlatform('web');
+
+    console.log('\nEnd processing');
+})
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
