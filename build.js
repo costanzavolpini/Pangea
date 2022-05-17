80d94b5d6849d89ec847333c60c5e7748074404c
Costanza Volpini <costanza.volpini@gmail.com>
2022-05-17 18:11:32 +0200

    Testing text with new parser

--- build.js
+++ build.js
@@ -1,7 +1,17 @@
-const StyleDictionaryPackage = require('style-dictionary');
+const StyleDictionary = require('style-dictionary');
+
+// Return something like "var(--color-blue)"
+function convertToVariableIfNeeded(value) {
+  if (value.startsWith("{") && value.endsWith("}")) {
+    return `var(--${value.slice(1, -1).replace(".", "-")})`;
+  }
+  return value;
+}
 
 // Auto generate Style Dictionary config file
-StyleDictionaryPackage.registerFormat({
+
+// Color Format
+StyleDictionary.registerFormat({
     name: 'scss/variables',
     formatter: function (dictionary, config) {
       return `${this.selector} {
@@ -10,9 +20,27 @@ StyleDictionaryPackage.registerFormat({
     }
   });  
 
+
+  // Text Format
+  StyleDictionary.registerFormat({
+    name: "scss/typography",
+    formatter: (dictionary, config) => (dictionary.allProperties.map((prop) => (`
+  .${prop.name} {
+    font: var(--${prop.name});
+    letter-spacing: ${convertToVariableIfNeeded(
+      prop.original.value.letterSpacing
+    )};
+    text-transform: ${convertToVariableIfNeeded(prop.original.value.textCase)};
+    text-decoration: ${convertToVariableIfNeeded(
+      prop.original.value.textDecoration
+    )};
+  }`)).join("\n"))
+  });
+
+
 // Transformer for Style Dictionary
 // Colors
-StyleDictionaryPackage.registerTransform({
+StyleDictionary.registerTransform({
     name: 'sizes/px',
     type: 'value',
     matcher: function(prop) {
@@ -32,28 +60,33 @@ const fontWeights = {
   'Bold': '700'
 }
 
-StyleDictionaryPackage.registerTransform({
+StyleDictionary.registerTransform({
   name: 'scss/typography',
   type: 'value',
-  matcher: function(token) {
-    return token.original.type === 'typography';
-  },
-  transformer: function(token) {
-    const typographyProps = token.original.value;
-    const typographyPropsArray = [];
+  transitive: true,
+  matcher: (token) => token.type === "typography",
+  transformer: (token) => {
 
-    for (let prop in typographyProps) {
-      if(prop === 'fontWeight') {
-        typographyPropsArray.push(`"${prop}": ${fontWeights[typographyProps[prop]]}`);
-      } else if (["lineHeight", "fontSize", "paragraphSpacing"].includes(prop)) {
-        typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}px`);
-      } else {
-        typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}`);
-      }
-    }
-    return `(${typographyPropsArray.join()})`;
+    const {fontWeight, fontSize, lineHeight, fontFamily} = token.original.value;
+    return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
+
+    // const typographyProps = token.original.value;
+    // const typographyPropsArray = [];
+
+    // for (let prop in typographyProps) {
+    //   if(prop === 'fontWeight') {
+    //     typographyPropsArray.push(`"${prop}": ${fontWeights[typographyProps[prop]]}`);
+    //   } else if (["lineHeight", "fontSize", "paragraphSpacing"].includes(prop)) {
+    //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}px`);
+    //   } else {
+    //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}`);
+    //   }
+    // }
+    // return `(${typographyPropsArray.join()})`;
   }
   });
+
+
 
 function getStyleDictionaryConfig(brand) {
   return {
