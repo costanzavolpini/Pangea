7d478cd0651dc20d92bdd84c07a3e933a463111a
Costanza Volpini <costanza.volpini@gmail.com>
2022-05-17 18:36:10 +0200

    Testing text

--- build.js
+++ build.js
@@ -1,14 +1,14 @@
 const StyleDictionaryPackage = require('style-dictionary');
 
-// // Return something like "var(--color-blue)"
-// function convertToVariableIfNeeded(value) {
-//   if (value.startsWith("{") && value.endsWith("}")) {
-//     return `var(--${value.slice(1, -1).replace(".", "-")})`;
-//   }
-//   return value;
-// }
-
 // Auto generate Style Dictionary config file
+
+const fontWeights = {
+  'Light': '200',
+  'Light Italic': '200',
+  'Regular': '400',
+  'Italic': '400',
+  'Bold': '700'
+}
 
 // Color Format
 StyleDictionaryPackage.registerFormat({
@@ -20,71 +20,19 @@ StyleDictionaryPackage.registerFormat({
     }
   });  
 
-
-  // // Text Format
-  // StyleDictionaryPackage.registerFormat({
-  //   name: "scss/typography",
-  //   formatter: (dictionary, config) => (dictionary.allProperties.map((prop) => (`
-  // .${prop.name} {
-  //   font: var(--${prop.name});
-  //   letter-spacing: ${convertToVariableIfNeeded(
-  //     prop.original.value.letterSpacing
-  //   )};
-  //   text-transform: ${convertToVariableIfNeeded(prop.original.value.textCase)};
-  //   text-decoration: ${convertToVariableIfNeeded(
-  //     prop.original.value.textDecoration
-  //   )};
-  // }`)).join("\n"))
-  // });
-
-
 // Transformer for Style Dictionary
-// Colors
 StyleDictionaryPackage.registerTransform({
     name: 'sizes/px',
     type: 'value',
     matcher: function(prop) {
-        return ["fontSize", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
+        return ["fontSize", "paragraphSpacing", "lineHeight", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
     },
     transformer: function(prop) {
+        console.log("prop", prop)
+        if(prop === 'fontWeight') return fontWeights[prop]; // replace "Bold/Regular/etc." with weight
         return parseFloat(prop.original.value) + 'px';
     }
     });
-
-// // Texts
-// const fontWeights = {
-//   'Light': '200',
-//   'Light Italic': '200',
-//   'Regular': '400',
-//   'Italic': '400',
-//   'Bold': '700'
-// }
-
-// StyleDictionaryPackage.registerTransform({
-//   name: 'scss/typography',
-//   type: 'value',
-//   transitive: true,
-//   matcher: (token) => token.type === "typography",
-//   transformer: (token) => {
-
-//     const {fontWeight, fontSize, lineHeight, fontFamily} = token.original.value;
-//     return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
-
-//     // const typographyProps = token.original.value;
-//     // const typographyPropsArray = [];
-
-//     // for (let prop in typographyProps) {
-//     //   if(prop === 'fontWeight') {
-//     //     typographyPropsArray.push(`"${prop}": ${fontWeights[typographyProps[prop]]}`);
-//     //   } else if (["lineHeight", "fontSize", "paragraphSpacing"].includes(prop)) {
-//     //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}px`);
-//     //   } else {
-//     //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}`);
-//     //   }
-//     // }
-//     // return `(${typographyPropsArray.join()})`;
-//   }
-//   });
 
 
 
