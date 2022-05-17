7de19a58517bafb27373a1537e7c0426e27e207e
Costanza Volpini <costanza.volpini@gmail.com>
2022-05-17 18:22:44 +0200

    Testing with expanded typography

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
 const StyleDictionaryPackage = require('style-dictionary');
 
-// Return something like "var(--color-blue)"
-function convertToVariableIfNeeded(value) {
-  if (value.startsWith("{") && value.endsWith("}")) {
-    return `var(--${value.slice(1, -1).replace(".", "-")})`;
-  }
-  return value;
-}
+// // Return something like "var(--color-blue)"
+// function convertToVariableIfNeeded(value) {
+//   if (value.startsWith("{") && value.endsWith("}")) {
+//     return `var(--${value.slice(1, -1).replace(".", "-")})`;
+//   }
+//   return value;
+// }
 
 // Auto generate Style Dictionary config file
 
@@ -21,21 +21,21 @@ StyleDictionaryPackage.registerFormat({
   });  
 
 
-  // Text Format
-  StyleDictionaryPackage.registerFormat({
-    name: "scss/typography",
-    formatter: (dictionary, config) => (dictionary.allProperties.map((prop) => (`
-  .${prop.name} {
-    font: var(--${prop.name});
-    letter-spacing: ${convertToVariableIfNeeded(
-      prop.original.value.letterSpacing
-    )};
-    text-transform: ${convertToVariableIfNeeded(prop.original.value.textCase)};
-    text-decoration: ${convertToVariableIfNeeded(
-      prop.original.value.textDecoration
-    )};
-  }`)).join("\n"))
-  });
+  // // Text Format
+  // StyleDictionaryPackage.registerFormat({
+  //   name: "scss/typography",
+  //   formatter: (dictionary, config) => (dictionary.allProperties.map((prop) => (`
+  // .${prop.name} {
+  //   font: var(--${prop.name});
+  //   letter-spacing: ${convertToVariableIfNeeded(
+  //     prop.original.value.letterSpacing
+  //   )};
+  //   text-transform: ${convertToVariableIfNeeded(prop.original.value.textCase)};
+  //   text-decoration: ${convertToVariableIfNeeded(
+  //     prop.original.value.textDecoration
+  //   )};
+  // }`)).join("\n"))
+  // });
 
 
 // Transformer for Style Dictionary
@@ -51,40 +51,40 @@ StyleDictionaryPackage.registerTransform({
     }
     });
 
-// Texts
-const fontWeights = {
-  'Light': '200',
-  'Light Italic': '200',
-  'Regular': '400',
-  'Italic': '400',
-  'Bold': '700'
-}
+// // Texts
+// const fontWeights = {
+//   'Light': '200',
+//   'Light Italic': '200',
+//   'Regular': '400',
+//   'Italic': '400',
+//   'Bold': '700'
+// }
 
-StyleDictionaryPackage.registerTransform({
-  name: 'scss/typography',
-  type: 'value',
-  transitive: true,
-  matcher: (token) => token.type === "typography",
-  transformer: (token) => {
+// StyleDictionaryPackage.registerTransform({
+//   name: 'scss/typography',
+//   type: 'value',
+//   transitive: true,
+//   matcher: (token) => token.type === "typography",
+//   transformer: (token) => {
 
-    const {fontWeight, fontSize, lineHeight, fontFamily} = token.original.value;
-    return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
+//     const {fontWeight, fontSize, lineHeight, fontFamily} = token.original.value;
+//     return `${fontWeight} ${fontSize}/${lineHeight} ${fontFamily}`;
 
-    // const typographyProps = token.original.value;
-    // const typographyPropsArray = [];
+//     // const typographyProps = token.original.value;
+//     // const typographyPropsArray = [];
 
-    // for (let prop in typographyProps) {
-    //   if(prop === 'fontWeight') {
-    //     typographyPropsArray.push(`"${prop}": ${fontWeights[typographyProps[prop]]}`);
-    //   } else if (["lineHeight", "fontSize", "paragraphSpacing"].includes(prop)) {
-    //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}px`);
-    //   } else {
-    //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}`);
-    //   }
-    // }
-    // return `(${typographyPropsArray.join()})`;
-  }
-  });
+//     // for (let prop in typographyProps) {
+//     //   if(prop === 'fontWeight') {
+//     //     typographyPropsArray.push(`"${prop}": ${fontWeights[typographyProps[prop]]}`);
+//     //   } else if (["lineHeight", "fontSize", "paragraphSpacing"].includes(prop)) {
+//     //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}px`);
+//     //   } else {
+//     //     typographyPropsArray.push(`"${prop}": ${typographyProps[prop]}`);
+//     //   }
+//     // }
+//     // return `(${typographyPropsArray.join()})`;
+//   }
+//   });
 
 
 
